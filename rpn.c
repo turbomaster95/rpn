4e1a476aae7f1cc15adcf9c855857923b0349747
Deva Midhun <admin@coder.is-a.dev>
2026-08-21 05:20:59 +0530

    upd

--- rpn.c
+++ rpn.c
@@ -5,93 +5,161 @@
 #include <limits.h>
 
 enum Tokens {
-  TOK_UNK = 0,
-  TOK_ADD,
-  TOK_SUB,
-  TOK_INT,
-  TOK_CHR
+    TOK_UNK = 0,
+    TOK_ADD,
+    TOK_SUB,
+    TOK_INT,
+    TOK_CHR
 };
 
 typedef struct Token {
-  int type;
-  int intg;
-  char chr;
+    int type;
+    int intg;
+    char chr;
 } Token;
 
-int pop(int *stack, int *stackc) {
+int pop(int *stack, int *stackc, int *result) {
     if (*stackc == 0) {
         return 0;
     }
 
     (*stackc)--;
-    return stack[*stackc];
+    *result = stack[*stackc];
+
+    return 1;
 }
 
-#define PUSH(x)             \
-    do {                    \
-        stack[stackc] = (x); \
-        stackc++;           \
-    } while (0)
+int push(int *stack, int *stackc, int value, int capacity) {
+    if (*stackc >= capacity) {
+        return 0;
+    }
 
-#define POP() pop(stack, &stackc);
+    stack[*stackc] = value;
+    (*stackc)++;
+
+    return 1;
+}
+
+void print_stack(const int *stack, int stackc, int capacity) {
+    printf("[");
+
+    for (int i = 0; i < stackc; i++) {
+        if (i > 0) {
+            printf("|");
+        }
+
+        printf("%d", stack[i]);
+    }
+
+    if (stackc < capacity) {
+        if (stackc > 0) {
+            printf("|");
+        }
+
+        printf("...");
+    }
+
+    printf("]\n");
+}
 
 int main(void) {
     char input[128];
     Token tokens[256];
-    memset(tokens, 0, sizeof(tokens));
     int pos = 0;
-
-    printf("!\n");
-
     int stack[256];
     int stackc = 0;
+
+    memset(tokens, 0, sizeof(tokens));
     memset(stack, 0, sizeof(stack));
+
+    printf("!\n");
 
     while (printf("> "), fgets(input, sizeof(input), stdin) != NULL) {
         char *p = strtok(input, " \t\n");
 
         while (p != NULL) {
-          if (pos >= sizeof(tokens) / sizeof(tokens[0])) {
-            fprintf(stderr, "Too many tokens\n");
-            return 1;
-          }
+            if (pos >= (int)(sizeof(tokens) / sizeof(tokens[0]))) {
+                fprintf(stderr, "!!! Too many tokens\n");
+                return 1;
+            }
 
-          if (strcmp(p, "+") == 0) {
-//              printf("add\n");
-              tokens[pos] = (Token){ .type = TOK_ADD };
-	      int right = POP();
-	      int left = POP();
-	      printf("%d\n", left + right);
-              pos++;
-          } else {
-              char *endptr;
-              long value;
+            if (strcmp(p, "[]") == 0) {
+                print_stack(stack, stackc, sizeof(stack) / sizeof(stack[0]));
+            } else if (strcmp(p, "+") == 0) {
+                int right;
+                int left;
 
-              errno = 0;
-              endptr = NULL;
-              value = strtol(p, &endptr, 10);
+                tokens[pos] = (Token){ .type = TOK_ADD };
+                pos++;
 
-              if (endptr != p && *endptr == '\0' && errno != ERANGE && value >= INT_MIN &&  value <= INT_MAX) {
-  //                printf("digit\n");
-                  tokens[pos] = (Token){
-                      .type = TOK_INT,
-                      .intg = (int)value
-                  };
-		  PUSH((int)value);
-                  pos++;
-              } else {
-                  printf("else, %s\n", p);
-		  tokens[pos] = (Token){
+                if (stackc < 2) {
+                    fprintf(stderr, "?? ops -> +\n");
+                } else {
+                    pop(stack, &stackc, &right);
+                    pop(stack, &stackc, &left);
+
+                    if (!push(stack, &stackc, left + right,
+                               sizeof(stack) / sizeof(stack[0]))) {
+                        fprintf(stderr, "[||///\n");
+                        return 1;
+                    }
+
+                    printf("%d\n", stack[stackc - 1]);
+                }
+            } else if (strcmp(p, "dup") == 0) {
+                tokens[pos] = (Token){ .type = TOK_UNK };
+                pos++;
+
+                if (stackc == 0) {
+                    fprintf(stderr, "[] --/--> 2[] ==> [0|0|0|0|0|0]\n");
+                } else if (stackc >= (int)(sizeof(stack) / sizeof(stack[0]))) {
+                    fprintf(stderr, "[||///\n");
+                    return 1;
+                } else {
+                    push(stack, &stackc, stack[stackc - 1],
+                         sizeof(stack) / sizeof(stack[0]));
+                }
+            } else if (strcmp(p, ".e") == 0) {
+                return EXIT_SUCCESS;
+            } else {
+                char *endptr;
+                long value;
+
+                errno = 0;
+                value = strtol(p, &endptr, 10);
+
+                if (
+                    endptr != p &&
+                    *endptr == '\0' &&
+                    errno != ERANGE &&
+                    value >= INT_MIN &&
+                    value <= INT_MAX
+                ) {
+                    tokens[pos] = (Token){
+                        .type = TOK_INT,
+                        .intg = (int)value
+                    };
+                    pos++;
+
+                    if (!push(stack, &stackc, (int)value,
+                               sizeof(stack) / sizeof(stack[0]))) {
+                        fprintf(stderr, "[||///\n");
+                        return 1;
+                    }
+                } else {
+                    printf("???, %s\n", p);
+
+                    tokens[pos] = (Token){
                         .type = TOK_UNK,
-			.chr = (char)p
-                  };
-                  pos++;
-              }
-	  }
+                        .chr = p[0]
+                    };
+                    pos++;
+                }
+            }
 
-          p = strtok(NULL, " \t\n");
+            p = strtok(NULL, " \t\n");
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
